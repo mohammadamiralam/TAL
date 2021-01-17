cbbb5defab8136723c55dbbf09c7caa312688598
Mohammad Amir Alam <amir.alam@hotmail.com>
2021-01-17 22:13:38 +1100

    infrastructure layer with seed data

---
+++ TAL.Infrastructure/Data/OccupationRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Tal.Core.Interfaces.Repo;
+using TAL.Core.ViewModels;
+
+namespace TAL.Infrastructure.Data
+{
+    public class OccupationRepository : IOccupationRepository
+    {
+        private readonly IEnumerable<Occupation> _occupations = null;
+        public OccupationRepository()
+        {
+            List<Occupation> occupations = new List<Occupation>();
+            occupations.Add(new Occupation() { Id = 1, Name = "Cleaner", RatingId = 3 });
+            occupations.Add(new Occupation() { Id = 2, Name = "Doctor", RatingId = 1 });
+            occupations.Add(new Occupation() { Id = 3, Name = "Author", RatingId = 2 });
+            occupations.Add(new Occupation() { Id = 4, Name = "Farmer", RatingId = 4 });
+            occupations.Add(new Occupation() { Id = 5, Name = "Mechanic", RatingId = 4 });
+            occupations.Add(new Occupation() { Id = 6, Name = "Florist", RatingId = 3 });
+            _occupations = occupations;
+        }
+
+        public Occupation GetOccupation(int id)
+        {
+            return _occupations.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public IEnumerable<Occupation> GetOccupations()
+        {
+            try
+            {
+                var occupation = _occupations
+                    .ToList();
+                return occupation;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ TAL.Infrastructure/Data/RatingRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Tal.Core.Interfaces.Repo;
+using TAL.Core.ViewModels;
+
+namespace TAL.Infrastructure.Data
+{
+    public class RatingRepository : IRatingRepository
+    {
+        private readonly IEnumerable<Rating> _ratings = null;
+        public RatingRepository()
+        {
+            List<Rating> ratings = new List<Rating>();
+            ratings.Add(new Rating() { Id = 1, Name = "Professional", Factor = 1.00 });
+            ratings.Add(new Rating() { Id = 2, Name = "White Collar", Factor = 1.25 });
+            ratings.Add(new Rating() { Id = 3, Name = "Light Manual", Factor = 1.50 });
+            ratings.Add(new Rating() { Id = 4, Name = "Heavy Manual", Factor = 1.75 });
+            _ratings = ratings;
+        }
+
+        public Rating GetRating(int id)
+        {
+            return _ratings.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public IEnumerable<Rating> GetRatings()
+        {
+            try
+            {
+                return _ratings.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
